6d8d0b4d4b7ca084f9e27f685c3a09abb19874d4
Jan Karas <karas.jan2@gmail.com>
2026-07-19 12:00:34 +0200

    chore: invalidate old quiz cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pravda-kviz-v8';
+const CACHE = 'pravda-kviz-v9';
 const CORE = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
