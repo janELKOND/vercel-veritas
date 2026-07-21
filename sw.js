71104da32d6c0773bd8572c576ca1fb145f2e310
Jan Karas <karas.jan2@gmail.com>
2026-07-21 20:16:25 +0200

    Bump quiz cache to v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pravda-kviz-v9';
+const CACHE = 'pravda-kviz-v10';
 const CORE = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
